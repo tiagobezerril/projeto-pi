04ae905f889c8ef01d71d8fdd8e0e4698900d7f4
beagareno <bhreno.barboza@sptech.school>
2024-10-10 16:28:51 -0300

    Atualização de BD

--- Codigo/Banco de dados/Bano_Dados_1.sql
+++ Codigo/Banco de dados/Bano_Dados_1.sql
@@ -1,66 +1,47 @@
-	CREATE DATABASE Safeware;
-    
-    USE Safeware;
-    
-    CREATE TABLE Empresa (
-    idEmpresa int primary key auto_increment,
-    nome varchar (45),
-    cnpj char (11)
-    );
-    
-    CREATE TABLE usuario (
-    idUsuario int primary key auto_increment,
-    nome varchar (45),
-    email varchar (45),
-    senhar varchar (11),
-    fkEmpresa int,
-    constraint fkClienteEmpresa foreign key (fkEmpresa) 
-    references Empresa (idEmpresa)
-    );
-    
-    CREATE TABLE manutencao (
-    idmanuntecao int primary key auto_increment,
-    dtinstalacao date,
-    manutencao date,
-    status varchar (10),
-    fkempresa int,
-    constraint fkClienteUsuario foreign key (fkEmpresa)
-    references Empresa (idEmpresa)
-    );
-    
-	CREATE TABLE dados (
-    idDados int primary key auto_increment,
-    dtColeta date,
-    porcentagem int,
-    fkmanutencao int,
-    Constraint fksensorManuntencao foreign key (fkmanutencao)
-    references manutencao (idmanuntecao)
-    );
-    
-    desc manutencao;
-  
-INSERT INTO empresa value
-	(default, 'Safeware', 12345678921);
-  
-INSERT INTO usuario value 
-	(default, 'Tiago', 'bezerriltiago@gmail.com', 'ab1234', default);
-  
-  select * from empresa;
-  update usuario set fkempresa = 1
-	where idusuario = 1;
-    
- SELECT u1.nome as usuario,
-		e1.nome as empresa
-	FROM usuario as u1
-    JOIN empresa as e1
-    on e1.idEmpresa = u1.fkEmpresa;
-    
-SELECT m1.status as Atividade,
-		p1.porcentagem as '%'
-	FROM manutencao as m1
-    JOIN dados as p1
-    ON m1.idmanuntecao = p1.fkmanutencao;
-    
-INSERT INTO manutencao VALUE 
-	(default, 2024-16-09, 2024-23-09, null, default) 
-    
+CREATE DATABASE safeware;
+use safeware;
+
+-- Criando a tabela Empresa
+CREATE TABLE Empresa (
+  idCadastro INT PRIMARY KEY,
+  razao_social VARCHAR(45),
+  nome_fantacia VARCHAR(45),
+  cnpj CHAR(14),
+  representante VARCHAR(45),
+  e_mail VARCHAR(45),
+  senha VARCHAR(45)
+);
+
+-- Criando a tabela Endereco
+CREATE TABLE Endereco (
+  idendereco INT PRIMARY KEY,
+  Estado VARCHAR(45),
+  cidade VARCHAR(45),
+  bairro VARCHAR(45),
+  logradouro VARCHAR(45),
+  numero CHAR(5),
+  cep CHAR(8),
+  fkempresa INT,
+  FOREIGN KEY (fkempresa) REFERENCES Empresa(idCadastro)
+);
+
+-- Criando a tabela Sensor
+CREATE TABLE Sensor (
+  idsensor INT PRIMARY KEY,
+  tipo VARCHAR(3),
+  dtinstalacao DATE,
+  status VARCHAR(11),
+  fkempresa INT,
+  FOREIGN KEY (fkempresa) REFERENCES Empresa(idCadastro)
+);
+
+-- Criando a tabela Dados
+CREATE TABLE Dados (
+  idDados INT PRIMARY KEY,
+  porcentagem INT,
+  dtdecoleta datetime,
+  fksensor INT,
+  FOREIGN KEY (fksensor) REFERENCES Sensor(idsensor)
+);
+
+SELECT current_timestamp();
