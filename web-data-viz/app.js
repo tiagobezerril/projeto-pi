42447810fc38baac43c8a6d3e297b32c2c163b46
ERIKCECILIO <erik.mendonca@sptech.school>
2024-11-27 19:51:56 -0300

    botao manutencao math random implementado, resolver vetor dinamico

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -1,11 +1,16 @@
 // var ambiente_processo = 'desenvolvimento';
 var ambiente_processo = 'desenvolvimento';
 
+
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 // Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
 // A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
 
 require("dotenv").config({ path: caminho_env });
+
+
+
+var bodyParser = require('body-parser');
 
 var express = require("express");
 var cors = require("cors");
