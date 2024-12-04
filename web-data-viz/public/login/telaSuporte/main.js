86341467a97a49616e34e0f80dbdb8d4b29056eb
vivids12 <viviane.santos@sptech.school>
2024-12-04 13:04:29 -0300

    ambiente de producao

--- web-data-viz/public/login/telaSuporte/main.js
+++ web-data-viz/public/login/telaSuporte/main.js
@@ -39,7 +39,7 @@ app.listen(PORTA_SERVIDOR, () => {
         ######   ####  #####  ### #     # 
         `
     );
-    console.info(`A API BobIA iniciada, acesse http://http://10.18.6.60:${PORTA_SERVIDOR}`);
+    console.info(`A API BobIA iniciada, acesse http://10.18.6.60:${PORTA_SERVIDOR}`);
 });
 
 // rota para receber perguntas e gerar respostas
